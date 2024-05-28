2c57900ab44be544f655349861f3d8c9734f9a1f
wooweh <wooweh@proton.me>
2024-05-28 09:09:10 +0200

    Setup Redux store.

---
+++ src/app/store.ts
@@ -0,0 +1,46 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist"
+import { coreSlice } from "../features/core/coreSlice"
+import storageSession from "redux-persist/lib/storage/session"
+import { adminSlice } from "../features/admin/adminSlice"
+
+const rootReducer = combineSlices(coreSlice, adminSlice)
+
+export type RootState = ReturnType<typeof rootReducer>
+
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+  },
+})
+export const persistor = persistStore(store)
+
+export type AppStore = typeof store
+export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
