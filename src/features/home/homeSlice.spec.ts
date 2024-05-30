7adaff3b8c2d905cf8e1c3f325093551a636613a
wooweh <wooweh@proton.me>
2024-05-30 11:55:58 +0200

    Setup home feature template.

---
+++ src/features/home/homeSlice.spec.ts
@@ -0,0 +1,7 @@
+import { describe } from "vitest"
+/*
+
+
+
+*/
+describe("home slice", () => {})
---
+++ src/features/home/homeSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
+import _ from "lodash"
+/*
+
+
+
+*/
+/*
+
+
+
+*/
+export interface HomeSliceState {}
+
+const initialState: HomeSliceState = {}
+
+export const homeSlice = createSlice({
+  name: "home",
+  initialState,
+  reducers: {
+    reducer: state => {},
+  },
+})
+
+export const {} = homeSlice.actions
+
+export default homeSlice.reducer
